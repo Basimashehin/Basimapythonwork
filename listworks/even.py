032a7fd149172704db0602445c51b2b6746cd3a3
Basima shehin <basimak95@gmail.com>
2022-06-10 09:18:49 +0400

    listworks

---
+++ listworks/even.py
@@ -0,0 +1,55 @@
+numbers=[12,13,14,15,16,17,18]
+
+
+# for num in numbers:
+#     if num%2==0:
+#         print(num)
+
+
+# printnum+1 ifnum>15  else print num-1 num=15 if num=15
+
+#
+# for num in numbers:
+#     if num>15 :
+#         print(num+1)
+#     elif num<15:
+#         print(num-1)
+#     else:
+#         print(num)
+
+
+# prinnt count of numbers in range 14-18
+
+
+# for num in numbers:
+#     if num>=14 and num<=18:
+#         count+=1
+#         print(num)
+
+# print(count)
+# count=0
+# for num in numbers:
+#     if num in range(14,19) :
+#         count+=1
+#
+#
+# print(count)
+
+
+numbers=[-1,2,0,3,4,5,-2,0,3,4,-5,0,7,0]
+neg_count=0
+po_count=0
+zero_count=0
+
+for num in numbers:
+    if num>0:
+        po_count+=1
+    elif num<0 :
+        neg_count+=1
+    else:
+        zero_count+=1
+
+
+print(f"positive number count is {po_count}")
+print(f"negetive number count is {neg_count}")
+print(f"zero number count is {zero_count}")
---
+++ listworks/lsdemo.py
@@ -0,0 +1,28 @@
+# list
+# set
+# tuple
+# dict
+#
+# arrays?
+# same type of data
+
+
+expense=[12000,25000,30000,40000]
+print(expense)
+
+
+st={"hai","hello",200,300,"four"}
+print(st)
+
+
+print(expense[0])
+print(expense[-1])
+i=0
+for i in range(0,len(expense)):
+    print(expense[i])
+
+
+for amount in expense:
+    print(expense)
+
+print(len(expense))
